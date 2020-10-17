24ac24346373e9955bf2facc93dd61b3d8fa30df
DanielMadden <danielfpjobs@gmail.com>
2020-10-17 12:06:42 -0600

    yeet

--- main.js
+++ main.js
@@ -305,7 +305,12 @@ sortContacts(sortType)
 /**
  * What I learned/reviewed through research and reinforced by customizing the project:
  *
- *    - How to create and altercate an array
- *    - How to save and load an array from local storage by properly using the JSON functions
- *    -
+ *    - How to create and altercate an object-filled array
+ *    - How to save and load an object-filled array from local storage by properly using the JSON functions
+ *    - How to read and use conditional operators
+ *    - How to input data into the function when calling it
+ *    - How to retrieve data placed into the function
+ *    - How to sort through each array's value
+ *    - How to retrieve form data using the .target method
+ *
  */
